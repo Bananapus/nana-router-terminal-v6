64ff10a1e4cf89c76b45f3d2acdccbcfc166160b
Jango <me.jango@protonmail.com>
2026-05-26 23:50:48 -0300

    chore: pin forwarding check pragma

--- src/libraries/JBForwardingCheck.sol
+++ src/libraries/JBForwardingCheck.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+pragma solidity 0.8.28;
 
 import {IJBTerminal} from "@bananapus/core-v6/src/interfaces/IJBTerminal.sol";
 import {IJBForwardingTerminal} from "../interfaces/IJBForwardingTerminal.sol";
